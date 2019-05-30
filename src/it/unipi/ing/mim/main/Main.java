129859222087995e5e9cc580df303f22af40bd94
Giuliano Peraz <giuliano.peraz@gmail.com>
2019-05-30 17:37:28 +0200

    Revert "Working testing code for matlab engine"
    This reverts commit 236c5f14ac60fda726504c81579593b1f23e7509.

--- src/it/unipi/ing/mim/main/Main.java
+++ src/it/unipi/ing/mim/main/Main.java
@@ -1,25 +1,10 @@
 package it.unipi.ing.mim.main;
-import java.util.concurrent.ExecutionException;
 
-import com.mathworks.engine.*;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        MatlabEngine eng;
-		try {
-			eng = MatlabEngine.startMatlab();
-			eng.putVariable("x", 7.0);
-	        eng.putVariable("y", 3.0);
-	        eng.eval("z = complex(x, y);");
-	        double x = eng.getVariable("x");
-	        System.out.println(x);
-		} catch (EngineException | IllegalArgumentException | IllegalStateException | InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
 	}
+
 }

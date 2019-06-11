a53eadc78e8549dd5540e3bc3398e8df4b2f060b
Giuliano Peraz <giuliano.peraz@gmail.com>
2019-06-11 15:55:38 +0200

    No more static calling. Now we use objects correctly

--- src/it/unipi/ing/mim/main/Main.java
+++ src/it/unipi/ing/mim/main/Main.java
@@ -1,6 +1,5 @@
 package it.unipi.ing.mim.main;
 
-import it.unipi.ing.mim.deep.Parameters;
 import it.unipi.ing.mim.img.elasticsearch.ElasticImgIndexing;
 import it.unipi.ing.mim.img.elasticsearch.ElasticImgSearching;
 
@@ -10,19 +9,19 @@ public class Main {
 		// TODO Chiedi all'utente se indicizzazione o query
 		System.out.println("Ricerca o indicizzazione? (R/I)");
 		//Scanner lineReader = new Scanner(System.in);
-		for (int i = 1; i < 2; ++i) {
+		for (int i = 0; i < 1; ++i) {
 			String ans = (i == 0)? "i":"r"; 
 			//lineReader.next();
 			//lineReader.close();
 
 			if (ans.toLowerCase().equals("r")) {
 				if(System.getProperty("os.name").startsWith("Windows"))
-					new ElasticImgSearching(Parameters.TOP_K_QUERY).search(".\\wikiartDEBUG\\ni-zan\\bamboo-and-elegant-stone.jpg");
+					new ElasticImgSearching(Parameters.TOP_K_QUERY).search("C:\\Users\\Matlab\\PaintingRecognitions\\testImgs\\photo5764702944778891453.jpg");
 				else
 					new ElasticImgSearching(Parameters.TOP_K_QUERY).search("wikiartDEBUG/ni-zan/autumn-wind-in-gemstones-trees.jpg");
 			}
 			else if (ans.toLowerCase().equals("i")) {
-				ElasticImgIndexing.indexAll(args);
+				new ElasticImgIndexing(Parameters.TOP_K_IDX).indexAll(args);;
 			}
 			else {
 				System.err.println("Not recognized option");

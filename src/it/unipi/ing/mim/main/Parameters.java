44c4643fa4a725a9e37d707f8fe6fa5cb09e81a3
valeriot90 <valtan@host-131-114-209-119.m.unipi.it>
2019-06-13 16:03:30 +0200

    Parameters modified

--- src/it/unipi/ing/mim/main/Parameters.java
+++ src/it/unipi/ing/mim/main/Parameters.java
@@ -17,20 +17,16 @@ public class Parameters {
 	public static final File SRC_FOLDER = new File("./");
 	
 	//k-Nearest Neighbors
-	public static final int KNN = 10;
+	public static final int K = 30;
 	
 	//Pivots File
 	public static final File PIVOTS_FILE = new File("pivot.dat");
 	
-
-	//Number Of Pivots
-	public static final Integer NUM_KMEANS_CLUSTER = 100;	// TODO Ricorda di cambiarlo
-
 	//Top K pivots For Indexing
 	public static final int TOP_K_IDX = 10;
 	
 	//Top K pivots For Searching
-	public static final int TOP_K_QUERY = 30;
+	public static final int TOP_K_QUERY = 10;
 	
 	//Lucene Index
 	public static final String INDEX_NAME = "painting";
@@ -52,16 +48,17 @@ public class Parameters {
 	public static final File IMAGE_NAMES_FILE =  new File("imageNames.dat");
 	
 	// Where are images to be indexed
-	public static final Path imgDir = FileSystems.getDefault().getPath(".\\wikiartDEBUG");
+	public static final Path imgDir = FileSystems.getDefault().getPath("./wikiartDEBUG");
 	
 	// Algorithm parameters
-	public static final int MAX_DISTANCE_THRESHOLD = 35;
+	public static final int MAX_DISTANCE_THRESHOLD = 34; // from 25 to 50
+	public static final Integer NUM_KMEANS_CLUSTER = 100;	// TODO Ricorda di cambiarlo
 	public static final int MIN_RANSAC_INLIERS = 12;
-	public static final int MIN_GOOD_MATCHES = 10;
+	public static final int MIN_GOOD_MATCHES = 15;
 	public static final double RANSAC_PX_THRESHOLD = 1.0;
 
 	// Number of random keypoint chosen from keypoint computed by SIFT
-	public static final int RANDOM_KEYPOINT_NUM = 100;
+	public static final int RANDOM_KEYPOINT_NUM = 200;
 	
 	// ORB feature extraction parameters
 	public static final int ORB_MAX_FEATURE = 1000;

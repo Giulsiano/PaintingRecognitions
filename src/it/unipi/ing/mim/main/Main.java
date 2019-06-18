7e4e6153c7ee30f46c31f7b4ab5c9a5ba7b7369d
Maria <Maria@host-131-114-208-176.m.unipi.it>
2019-06-18 17:49:32 +0200

    code updated from branchGiuls

--- src/it/unipi/ing/mim/main/Main.java
+++ src/it/unipi/ing/mim/main/Main.java
@@ -9,7 +9,7 @@ public class Main {
 		System.out.println("Ricerca o indicizzazione? (R/I)");
 		//Scanner lineReader = new Scanner(System.in);
 
-		for (int i = 0; i < 1; ++i) {
+		for (int i = 1; i < 2; ++i) {
 			String ans = (i == 0)? "i":"r"; 
 			//lineReader.next();
 			//lineReader.close();

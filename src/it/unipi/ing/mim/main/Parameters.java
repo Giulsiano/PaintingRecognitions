ae66e6713f70ebb0d8c99d06bd1c684158a52fc1
Giuliano Peraz <giuliano.peraz@gmail.com>
2019-06-20 18:33:43 +0200

    Changed names of file with info about clusters and Keypoints

--- src/it/unipi/ing/mim/main/Parameters.java
+++ src/it/unipi/ing/mim/main/Parameters.java
@@ -19,8 +19,6 @@ public class Parameters {
 	//k-Nearest Neighbors
 	public static final int KNN = 30;
 
-	//Pivots File
-	public static final File PIVOTS_FILE = new File("pivot.dat");
 	
 	//Top K pivots For Indexing
 	public static final int TOP_K_IDX = 10;
@@ -36,23 +34,14 @@ public class Parameters {
 	public static final File RESULTS_HTML = new File("out/deep.seq.html");
 	public static final File RESULTS_HTML_ELASTIC = new File("out/deep.elastic.html");
 	public static final File RESULTS_HTML_REORDERED = new File("out/deep.reordered.html");
-	
-	// Our parameters
-	// Files we need to do things
-	public static final File DB_PATH = new File("data/database.db");
-	public static final File CLUSTER_FILE = new File("clusters.dat");
-	public static final File LABEL_FILE = new File("labels.dat");
-	public static final File DESCRIPTOR_FILE = new File("descriptors.dat");
-	public static final File POSTING_LISTS_FILE =  new File("posting_lists.dat");
-	public static final File KEYPOINT_PER_IMAGE_FILE = new File("keyPerImage.dat");
-	public static final File IMAGE_NAMES_FILE =  new File("imageNames.dat");
-	
+		
 	// Where are images to be indexed
 	public static final Path imgDir = FileSystems.getDefault().getPath("./wikiartDEBUG");
 
+	// Our parameters
 	// Algorithm parameters
 	public static final int MAX_DISTANCE_THRESHOLD = 34; // from 25 to 50
-	public static final Integer NUM_KMEANS_CLUSTER = 2000;	// TODO Ricorda di cambiarlo
+	public static final Integer NUM_KMEANS_CLUSTER = 200;	// TODO Ricorda di cambiarlo
 	public static final int MIN_RANSAC_INLIERS = 12;
 	public static final int MIN_GOOD_MATCHES = 15;
 	public static final double RANSAC_PX_THRESHOLD = 1.0;
@@ -65,5 +54,14 @@ public class Parameters {
 	
 	// Megapixel per image
 	public static final int MPX_PER_IMAGE = 500000;
+	
+	// Files we need to do things
+	public static final File LABEL_FILE = new File("labels" + RANDOM_KEYPOINT_NUM + "x" + NUM_KMEANS_CLUSTER + ".dat");
+	public static final File DESCRIPTOR_FILE = new File("descriptors" + RANDOM_KEYPOINT_NUM + ".dat");
+	public static final File POSTING_LISTS_FILE =  new File("posting_lists" + RANDOM_KEYPOINT_NUM + "x" + NUM_KMEANS_CLUSTER + ".dat");
+	public static final File KEYPOINT_PER_IMAGE_FILE = new File(RANDOM_KEYPOINT_NUM + "keyPerImage.dat");
+	public static final File IMAGE_NAMES_FILE =  new File("imageNames.dat");
 
+	//Pivots File
+	public static final File PIVOTS_FILE = new File(RANDOM_KEYPOINT_NUM + "x" + NUM_KMEANS_CLUSTER + "pivot.dat");
 }

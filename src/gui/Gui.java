6c3d61cb23d4331d4f5ada93abb5e918f26f2de7
valeriot90 <valtan@host-131-114-208-196.m.unipi.it>
2019-06-27 12:39:00 +0200

    Modified Gui class with Ratio Button and calling to Main function

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -1,104 +1,90 @@
 package gui;
 
 import java.io.File;
-import java.nio.file.Path;
 
-import javax.swing.JFrame;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 
-import it.unipi.ing.mim.img.elasticsearch.ElasticImgIndexing;
-import it.unipi.ing.mim.img.elasticsearch.ElasticImgSearching;
-import it.unipi.ing.mim.main.Parameters;
+import it.unipi.ing.mim.main.Main;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
+import javafx.scene.paint.Color;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 
 public class Gui extends Application{
+	
+	double buttonh = 30;
+	double buttonw = 200;
+	int areaw = 20;
 
-	private JFrame frame;
-	JScrollPane sp;
-	Path absoluteImagePath = null;
+	String absoluteImagePath = "";
 	String absoluteImageFile = "";
-	JTextArea txtArea = new JTextArea(200, 200);
 	it.unipi.ing.mim.main.Parameters mypar = new it.unipi.ing.mim.main.Parameters();
+	String LABEL_COLOR = "#8bc34a";
 	
 	
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		//EventQueue.invokeLater(new Runnable() {
-
-		//PlatformImpl.startup(()->{});
-		
 		launch(args);
-		//new JFXPanel();
-//		Platform.runLater(new Runnable() {
-//			public void run() {
-//				try {
-//					Gui window = new Gui();
-//					window.frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//				Platform.exit();
-//			}
-//		});
 	}
-
-	/**
-	 * Create the application.
-	 */
-//	public Gui() {
-//		initialize();
-//	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize(Stage primaryStage) {
 		Button indbtn = new Button();
+		indbtn.setMaxSize(buttonw, buttonh);
+		indbtn.setMinSize(buttonw, buttonh);
 		indbtn.setText("Start Indexing");
 
 		Button srcbtn = new Button();
+		srcbtn.setPrefSize(buttonw, buttonh);
 		srcbtn.setText("Start Searching");
 
-		ScrollPane s1 = new ScrollPane();
+		TextField indexname = new TextField();
+		indexname.setText("index_name");
 		
-		Text indexname = new Text();
-		Text pathname = new Text();
+		TextField pathname = new TextField();
+		pathname.setText("Select path to index");
+		pathname.setAlignment(Pos.CENTER);
+		
+		TextField filename = new TextField();
+		filename.setText("Select file to search");
+		filename.setAlignment(Pos.CENTER);
+		
 		
         indbtn.setOnAction(new EventHandler<ActionEvent>() {
  
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Puppa forte World!");
-                selectpath(primaryStage);
+                //selectpath(primaryStage);
                 
                 if(!absoluteImagePath.equals("")) {
-					ElasticImgSearching eis;
-					try {
-						ElasticImgIndexing eii = new ElasticImgIndexing(mypar.TOP_K_IDX);
-						eii.indexAll(absoluteImagePath);
-						eii.close();
-					} catch (Exception e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
+                	Main.main(new String[]{"index", absoluteImagePath,"-i", indexname.getText()});
 				}
             }
         });
@@ -108,36 +94,73 @@ public class Gui extends Application{
             @Override
             public void handle(ActionEvent event) {
             	System.out.println("Search button pressed");
-				selectfile(primaryStage);
+				//selectfile(primaryStage);
 				
 				if(!absoluteImageFile.equals("")) {
-					ElasticImgSearching eis;
-					try {
-						eis = new ElasticImgSearching(mypar.TOP_K_QUERY);
-						eis.search(absoluteImageFile, false);
-						eis.close();
-					} catch (Exception e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
+					Main.main(new String[]{"search", absoluteImageFile,"-i", indexname.getText()});
 				}
             }
         });
-        
-        //TODO
-        indexname.setOnMouseClicked(new EventHandler<ActionEvent>() {
-        	 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Puppa forte World!");
-                selectpath(primaryStage);
                 
-            }
+        indexname.setOnMousePressed(EventHandler -> {
+    	    //System.out.println("plutto ");
+            //selectpath(primaryStage);
+    	    //indexname.setText("");
         });
         
-        txtArea.setEditable(false);
+        pathname.setOnMousePressed(EventHandler -> {
+    	    System.out.println("pippa ");
+            selectpath(primaryStage);
+            if(!absoluteImagePath.equals(""))
+            	pathname.setText(absoluteImagePath);
+        });
+        
+        filename.setOnMousePressed(EventHandler -> {
+    	    System.out.println("pippa ");
+            selectfile(primaryStage);
+            if(!absoluteImageFile.equals(""))
+            	filename.setText(absoluteImageFile);
+        });
+
+        //#8bc34a
+        Label l = new Label("Do you want see the result image?");
+        
+        l.setTextFill(Color.web(LABEL_COLOR));
+        l.setPrefHeight(50);
+        
+        ToggleGroup group = new ToggleGroup();
+        RadioButton rbyes = new RadioButton("yes");
+        RadioButton rbno = new RadioButton("no");
+        rbyes.setUserData("yes");
+        rbno.setUserData("no");
+        rbyes.setToggleGroup(group);
+        rbno.setToggleGroup(group);
+        rbyes.setSelected(false);
+        rbno.setSelected(true);
+        
+        group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+        	public void changed(ObservableValue<? extends Toggle> ov,
+        			Toggle old_toggle, Toggle new_toggle) {
+        		if(group.getSelectedToggle() !=null) {
+        			if(group.getSelectedToggle().getUserData().toString().equals("yes")) {
+        				Main.showMatchWindow = true;
+        			}
+        			else Main.showMatchWindow = false;
+        		}
+        	}
+        
+        });
         
         GridPane root = new GridPane();
+        Image img = new Image(new File("/Users/valeriotanferna/git/PaintingRecognitions/src/gui/background.png").toURI().toString());
+        BackgroundImage bgi = new BackgroundImage(
+        		img, 
+        		BackgroundRepeat.NO_REPEAT, 
+        		BackgroundRepeat.NO_REPEAT, 
+        		BackgroundPosition.DEFAULT, 
+        		new BackgroundSize(BackgroundSize.AUTO,BackgroundSize.AUTO, false, false, true, false));
+        root.setBackground(new Background(bgi));
+        root.setPrefSize(500, 500);
         root.setAlignment(Pos.CENTER);
         root.setHgap(10);
         root.setVgap(10);
@@ -145,109 +168,25 @@ public class Gui extends Application{
         
         
 
-        Scene scene = new Scene(root, 300, 275);
+        Scene scene = new Scene(root, 966, 652);
         primaryStage.setScene(scene);
         
         root.add(indexname, 0, 0);
-        root.add(indbtn, 1, 0);
         root.add(pathname, 0, 1);
-        root.add(srcbtn, 1, 1);
+        root.add(indbtn, 1, 1);
+        root.add(filename, 0, 2);
+        root.add(srcbtn, 1, 2);
+        root.add(l, 0, 3);
+        root.add(rbyes, 0, 4);
+        root.add(rbno, 1, 4);
 
         root.setGridLinesVisible(true);
-        
+
         primaryStage.setTitle("Painting Recognition");
         primaryStage.setScene(scene);
         primaryStage.show();
 	}
 
-        
-		
-		
-/////////////////////////////////////////////////////////////////////////////////////////		
-/*		final JFXPanel panel = new JFXPanel();
-		
-		frame = new JFrame();
-		frame.add(panel);
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JScrollPane sp = new JScrollPane(txtArea);
-		sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		txtArea.setEditable(false);
-		txtArea.setLineWrap(true);
-		txtArea.setWrapStyleWord(true);
-		
-		Button btnNewButton = new Button("Index");
-		btnNewButton.setOnAction(new EventHandler<ActionEvent>(){ //addActionListener(new ActionListener() {
-			public void handle(ActionEvent e) {
-				Main.test();
-					
-				ElasticImgIndexing eii;
-				try {
-					eii = new ElasticImgIndexing(mypar.TOP_K_IDX);
-					if(eii.isESIndexExist(mypar.INDEX_NAME)) {
-						//open new window
-						Alert alert = new Alert(AlertType.CONFIRMATION, "Delete " + mypar.INDEX_NAME + " ?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
-						alert.showAndWait();
-
-						if (alert.getResult() == ButtonType.YES) {
-						    //do stuff
-							System.out.println("Indice cancellato: puppa!");
-						}
-					}
-					
-				} catch (Exception e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				
-				txtArea.append("Start Indexing pressed\n");
-								
-				for(int i = 0; i<20; i++) {
-					txtArea.append("we uaglio'!\n");
-				}
-				txtArea.append("\n");
-				e.consume();
-			}
-		});
-
-		frame.getContentPane().setLayout(new FormLayout(new ColumnSpec[] {
-				ColumnSpec.decode("450px:grow"),},
-			new RowSpec[] {
-				RowSpec.decode("35px"),
-				RowSpec.decode("35px"),
-				RowSpec.decode("200px:grow"),}));
-		frame.getContentPane().add("1, 1, fill, top", btnNewButton);
-		frame.getContentPane().add(sp, "1, 3, fill, fill");
-		
-		Button btnNewButton_1 = new Button("Search");
-
-		btnNewButton_1.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent e) {
-				System.out.println("Search button pressed");
-				selectfile();
-				
-				if(!absoluteImagePath.equals("")) {
-					ElasticImgSearching eis;
-					try {
-						eis = new ElasticImgSearching(mypar.TOP_K_QUERY);
-						eis.search(absoluteImagePath, false);
-						eis.close();
-					} catch (Exception e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-				}
-				e.consume();
-			}
-		});
-		frame.getContentPane().add(btnNewButton_1, "1, 2, fill, center");
-		frame.add(btnNewButton_1, "1, 2, fill, center");
-
-		frame.pack();
-
-	}
-	*/
 	private void selectfile(Stage primaryStage) {
 		System.out.println("selectFile called");
 		FileChooser fileChooser = new FileChooser();
@@ -267,7 +206,7 @@ public class Gui extends Application{
 		File selectedDirectory = directoryChooser.showDialog(primaryStage);
 		
 		if (selectedDirectory != null) {
-			absoluteImagePath = selectedDirectory.toPath();
+			absoluteImagePath = selectedDirectory.getAbsolutePath();
 		}
 	}
 	
--- src/it/unipi/ing/mim/main/Main.java
+++ src/it/unipi/ing/mim/main/Main.java
@@ -1,38 +1,112 @@
 package it.unipi.ing.mim.main;
 
-import java.nio.file.Path;
+import static org.bytedeco.opencv.global.opencv_features2d.drawMatches;
+import static org.bytedeco.opencv.global.opencv_highgui.destroyAllWindows;
+import static org.bytedeco.opencv.global.opencv_highgui.waitKey;
 
+import java.util.Map;
+
+import org.bytedeco.opencv.opencv_core.DMatchVector;
+import org.bytedeco.opencv.opencv_core.KeyPointVector;
+import org.bytedeco.opencv.opencv_core.Mat;
+
+import com.github.cliftonlabs.json_simple.JsonObject;
+
+import it.unipi.ing.mim.deep.tools.Output;
+import it.unipi.ing.mim.features.BoundingBox;
 import it.unipi.ing.mim.img.elasticsearch.ElasticImgIndexing;
 import it.unipi.ing.mim.img.elasticsearch.ElasticImgSearching;
+import it.unipi.ing.mim.utils.MetadataRetriever;
 
 public class Main {
-	
-	public static void main(String[] args) throws Exception {
-		System.out.println("Ricerca o indicizzazione? (R/I)");
-		//Scanner lineReader = new Scanner(System.in);
+	public static boolean showMatchWindow = false;
+	public static void main(String[] args) {
+		if (args.length < 2) printHelp();
+		else {
+		    String indexName = "-i".equals(args[args.length - 2]) ? args[args.length - 1] :
+		                                                            Parameters.INDEX_NAME;
+			try {
+				switch (args[0]) {
+				    case "search":
+				        ElasticImgSearching eis = 
+				                        new ElasticImgSearching(Parameters.TOP_K_QUERY, indexName);
+				        String bestGoodMatch = eis.search(args[1]);
+				        eis.close();
+				        
+				        if (bestGoodMatch != null) {
+				            JsonObject metadata = MetadataRetriever.readJsonFile(bestGoodMatch);
+			                String qryImagePath = Parameters.BASE_URI + args[1];
+			                String bestMatchPath = Parameters.BASE_URI + bestGoodMatch;
+			                Output.toHTML(metadata, qryImagePath, bestMatchPath, Parameters.RESULTS_HTML);
+				        }
+				        if (showMatchWindow) {
+				            Mat imgMatches = new Mat();
+				            Map<String, Object> bestMatch = eis.getBestGoodMatch();
+				            Mat queryImg = (Mat) bestMatch.get("queryImg");
+				            KeyPointVector qryKeypoints = 
+				                    (KeyPointVector) bestMatch.get("queryKeypoints");
+				            Mat bestImg = (Mat) bestMatch.get("image");
+				            KeyPointVector bestKeyPoints = 
+                                    (KeyPointVector) bestMatch.get("imageKeypoints");
+				            DMatchVector matchVector = 
+				                    (DMatchVector) bestMatch.get("matchVector");
+				            Mat homomography = (Mat) bestMatch.get("homomography");
+				            drawMatches(queryImg, qryKeypoints, bestImg , bestKeyPoints, matchVector, imgMatches);
+				            BoundingBox.addBoundingBox(imgMatches, queryImg, homomography, 0);// queryImg.cols());
+				            BoundingBox.imshow("RANSAC", imgMatches);
+				            waitKey();
+				            destroyAllWindows();
+				        }
+				        break;
 
-		for (int i = 0; i < 2; ++i) {
-			String ans = (i == 0)? "i":"r"; 
-			//lineReader.next();
-			//lineReader.close();
-			
-			if (ans.toLowerCase().equals("r")) {
-				ElasticImgSearching eis = new ElasticImgSearching(Parameters.TOP_K_QUERY);
-				eis.search(args[0], false);
-				eis.close();
-			}
-			else if (ans.toLowerCase().equals("i")) {
-				ElasticImgIndexing eii = new ElasticImgIndexing(Parameters.TOP_K_IDX);
-				eii.indexAll(Parameters.imgDir);
-				eii.close();
-			}
-			else {
-				System.err.println("Not recognized option");
+
+				    case "index":
+				        ElasticImgIndexing eii = 
+			                            new ElasticImgIndexing(Parameters.TOP_K_IDX, indexName);
+				        eii.indexAll(args[1]);
+				        eii.close();
+				        break;
+
+				    default:
+				        printHelp();
+				        break;
+				}
+				System.out.println("Program ended");
+				System.exit(0);
+			} 
+			catch (Exception e) {
+				System.err.println("Program generated an exception: " + e.getClass().getName() 
+						+ ":\n" + e.getMessage() + "\nExiting");
+				System.exit(1);
 			}
 		}
-		System.out.println("Program ended");
 	}
 	
+	private static void printHelp() {
+		System.out.println("Available commands:");
+		System.out.println("search path/to/image [-i index_name]");
+		System.out.println("\t\tAsk the program to retrieve");
+		System.out.println("\t\tthe most similar image present");
+		System.out.println("\t\tinto the index called index_name");
+		System.out.println("\t\tinstead of default one (" + Parameters.INDEX_NAME +")");
+		System.out.println();
+		System.out.println("index dir [-i index_name]");
+		System.out.println("\t\tStart indexing each image into dir");
+		System.out.println("\t\tOptionally you can tell the program");
+		System.out.println("\t\tto index objects using index_name");
+		System.out.println("\t\tinstead of default one (" + Parameters.INDEX_NAME +").");
+		System.out.println("\t\tdir must contain subdirectories that");
+		System.out.println("\t\tcontains images to index");
+		System.out.println();
+		System.out.println("statistics -tp TPdir -tn TNdir [-i index_name]");
+		System.out.println("\t\tStart gathering statistics using");
+		System.out.println("\t\timages in TPdir to compute true");
+		System.out.println("\t\tpositives and images in TNdir for");
+		System.out.println("\t\tcomputing true negative. Optionally");
+		System.out.println("\t\tthe user can use index_name index");
+		System.out.println("\t\tinstead of default one (" + Parameters.INDEX_NAME +")");
+	}
+
 	public static void test() {
 		System.out.println("INDEXING FROM GUI");
 	}
